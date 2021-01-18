2777d80c85f50d304ba50008d491a49a34f1a1c6
堀田直也 <chokunari3@gmail.com>
2021-01-18 14:16:24 +0000

    json取得後、特定の要素にアクセス可能。

--- src/Field/Field.js
+++ src/Field/Field.js
@@ -1,17 +1,6 @@
 import React, { useState,useEffect } from 'react';
-//import { makeStyles } from '@material-ui/core/styles';
-//import Grid from '@material-ui/core/Grid';
-import SearchBox from '../SearchBox/SearchBox';
-
-/*
-const useStyles = makeStyles({
-  statuscard: {
-    position: 'relative',
-    top: 50,
-    left: 100,
-  },
-});
-*/
+//import SearchBox from '../SearchBox/SearchBox';
+import SelectField from '../SelectField/SelectField';
 
 //const defaultURL = "https://mediaarts-db.bunka.go.jp/api/search?fieldId=animation&categoryId=an-col&sort=date&limit=10"
 const defaultURL = 'http://api.moemoe.tokyo/anime/v1/master/2021/1';
@@ -24,26 +13,33 @@ export default function Field() {
 
   const [loading, setLoading] = useState(true);
   const [animeinfo, setAnimeInfo] = useState([]);
-  //const [errorMessage, setErrorMessage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
 
     useEffect(() => {
     fetch(defaultURL,{mode:'cors'})
       .then(response => response.json())
       .then(jsonobj => {
-        /*
+        
         let tmparray = [];
-        let tmparray2 = {title:"",public_url:"",city_name:""};
-        jsonobj.forEach((i)=>{
-            tmparray2= {
+        //let tmparray2 = {title:"",public_url:"",city_name:""};
+        console.log(jsonobj[1].title);
+        for(let i in jsonobj) {
+            //let tmparray2 = jsonobj[i].title;
+            
+            let tmparray2= {
                 title:jsonobj[i].title,
                 public_url:jsonobj[i].public_url,
                 city_name:jsonobj[i].city_name
             };
-            tmparray.push(tmparray2);
-        });
+            
+            tmparray.push(JSON.stringify(tmparray2));
+        };
         setAnimeInfo(tmparray);
-        */
-        setAnimeInfo([JSON.stringify(jsonobj)]);
+        
+        //const tmparray = JSON.stringify(jsonobj[0].title);
+        //setAnimeInfo(tmparray);
+        //console.log(tmparray);
+        //setAnimeInfo([JSON.stringify(jsonobj)]);
         setLoading(false);
       });
     });
@@ -64,7 +60,8 @@ export default function Field() {
 
   return (
     <React.Fragment>
-        <SearchBox search={search} />
+        {/*<SearchBox search={search} />*/}
+        <SelectField />
         {loading ? (
          <span>loading...</span>
          ) :(
