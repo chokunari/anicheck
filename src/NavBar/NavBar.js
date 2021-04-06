09a10502b93c7eda2cfa017e4b6ff3c5c9a9a141
堀田直也 <chokunari3@gmail.com>
2021-04-06 23:00:29 +0900

    update:リリースに向けて不要な部品をコメントアウト。

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -3,10 +3,10 @@ import { fade, makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+//import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import AccountCircleSharpIcon from '@material-ui/icons/AccountCircleSharp';
+//import AccountCircleSharpIcon from '@material-ui/icons/AccountCircleSharp';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
 
@@ -24,6 +24,7 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     color: "#fafafa"
   },
+  /*
   loginbutton: {
     backgroundColor: '#009688',
     right: 15,
@@ -38,6 +39,7 @@ const useStyles = makeStyles((theme) => ({
       backgroundColor: fade(theme.palette.common.white, 0.30),
     }
   },
+  */
 }));
 
 const theme = createMuiTheme({
@@ -46,7 +48,7 @@ const theme = createMuiTheme({
       main: "#00bfa5"
     },
     secondary: {
-      main: "#ffeb3b"
+      main: "#1de9b6"
     }
   }
 });
@@ -59,16 +61,20 @@ export default function NavBar() {
     <div className={classes.root}>
       <AppBar position="static" color="primary" className={classes.appBar}>
         <Toolbar>
+          {/*
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
+           */}
           <Typography variant="h6" className={classes.title}>
             Anicheck
           </Typography>
+          {/* いつかログインボタン実装する？
           <Button color="inherit" className={classes.loginbutton}>Login</Button>
           <Button className={classes.accountbutton}>
             <AccountCircleSharpIcon ></AccountCircleSharpIcon>
           </Button>
+          */}
         </Toolbar>
       </AppBar>
     </div>
