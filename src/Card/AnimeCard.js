0f0fb52a6ddc96e01f69ef128a4885aaa57314f9
堀田直也 <chokunari3@gmail.com>
2021-04-17 23:42:14 +0900

    update:CSS修正。collapseボタン実装。

--- src/Card/AnimeCard.js
+++ src/Card/AnimeCard.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -7,17 +8,38 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Collapse from '@material-ui/core/Collapse';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
-    //maxWidth: 250,
     maxWidth: "100%",
     height: "100%",
+    display: "flex",
+    flexDirection: "column",
   },
-});
+  cardButton: {
+    marginTop: "auto",
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+}));
 
 export default function AnimeCard(props) {
   const classes = useStyles();
+  const [expanded, setExpanded] = useState(false);
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
   return (
     <Card className={classes.root}>
       <CardActionArea 
@@ -46,22 +68,31 @@ export default function AnimeCard(props) {
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
               {`公式サイト：${props.public_url}`}
-              {/* 
-              <br/>
-              {props.city_name ? (
-                  `聖地：${props.city_name}`
-              ):(
-                  '聖地：データなし'
-              )}
-              */}
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
-        <Button size="large" color="primary" fullWidth="true">
+      
+      <CardActions className={classes.cardButton} disableSpacing>
+        <Button 
+          size="large" 
+          color="primary" 
+          fullWidth="true"
+          onClick={handleExpandClick}
+        >
           説明文(未実装)
+          <ExpandMoreIcon 
+            className={clsx(classes.expand, {
+              [classes.expandOpen]: expanded,
+            })}
+          />
         </Button>
       </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+            {/*{props.description}*/}
+            テスト
+        </CardContent>
+      </Collapse>
     </Card>
   );
 }
